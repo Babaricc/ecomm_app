cc73815d451214c8629a4e79b82c1aa4f127d5ce
unknown <github_username>
2023-05-23 17:48:07 +0500

    files added

---
+++ app/src/main/java/com/example/ecomm_app/AppIntro.java
@@ -0,0 +1,56 @@
+package com.example.ecomm_app;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class AppIntro extends com.github.paolorotolo.appintro.AppIntro {
+
+
+    @Nullable
+    @Override
+    public void init(Bundle savedInstanceState) {
+        addSlide(SampleSlide.newInstance(R.layout.slide1));
+        addSlide(SampleSlide.newInstance(R.layout.slide2));
+        addSlide(SampleSlide.newInstance(R.layout.slide3));
+
+        setIndicatorColor(Color.parseColor("#FF9800"), Color.parseColor("#855002"));
+        setColorSkipButton(Color.parseColor("#FF9800"));
+        setColorDoneText(Color.parseColor("#FF9800"));
+        setNextArrowColor(Color.parseColor("#FF9800"));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onDonePressed() {
+        gotoHome();
+    }
+
+    @Override
+    public void onSkipPressed() {
+        gotoHome();
+    }
+
+    public void gotoHome() {
+        startActivity(new Intent(AppIntro.this, HomeScreen.class));
+    }
+
+
+}
